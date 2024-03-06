782ed0701dd05491cd4d534825de79ebd1e1dc8e
hadasaGIT <hadasa0855@gmail.com>
2024-03-06 06:38:52 +0200

    update app.py

--- application/app.py
+++ application/app.py
@@ -16,21 +16,21 @@ def index():
 
 @app.route('/find_route', methods=['POST'])
 def find_route():
+    """
+    Handles POST requests to find the shortest route between two points.
+
+    :return: A rendered HTML template displaying the shortest route and the generated KML file path.
+    """
     start_point = request.form.get('start_point')
     end_point = request.form.get('end_point')
 
     if not start_point or not end_point:
-        return jsonify({'error': 'נקודת ההתחלה ונקודת הסיום הינן שדות חובה'}), 400
+        return jsonify({'error': 'Start and end points are required fields'}), 400
 
     short_path = shortest_path(ast.literal_eval(start_point), ast.literal_eval(end_point), graph_data)
-    print("Shortest distance:", short_path)
-
-    response = {
-        'shortest_path': short_path
-    }
     kml_file_path = generate_kml(short_path)
-    print("KML file generated:", kml_file_path)
-    return render_template('result.html', shortest_route=response,kml_file_path=kml_file_path)
+
+    return render_template('result.html', shortest_route=short_path, kml_file_path=kml_file_path)
 
 
 if __name__ == '__main__':
